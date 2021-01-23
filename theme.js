bed48deea396e9d70355775fdb091cd475cef49a
Ludovico Cammarata <60526281+frosty04@users.noreply.github.com>
2021-01-23 19:20:22 +0100

    Added another theme

--- theme.js
+++ theme.js
@@ -2,17 +2,14 @@ let schemes = [
     ["Dark", "#121212", "#BDBDBD", "#616161"],
     ["Light", "#EEEEEE", "#000000", "#757575"],
     ["Orange", "#4C7394", "#BB9ED1", "#FAB29E"], 
+    ["Ocean", "#E3F2FD", "#01579B", "#03A9F4"],
+    ["Nature", "#E8F5E9", "#2E7D32", "#4CAF50"],
+    ["Pony", "#FCE4EC", "#AD1457", "#E91E63"],    
     ];
 
     /* first is the background color
        second is the text color
-       third is the link color
-
-       Other Themes:
-       ["Ocean", "#E3F2FD", "#01579B", "#03A9F4"],
-       ["Nature", "#E8F5E9", "#2E7D32", "#4CAF50"],
-       ["Pony", "#FCE4EC", "#AD1457", "#E91E63"],
-    */
+       third is the link color*/
     
 let current = (localStorage.getItem("scheme") || 0) - 1;
 changePalette();
